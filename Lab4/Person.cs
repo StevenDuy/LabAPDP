b272540f17c8c358b4ad6721c3767e4f0c3213d5
StevenDuy <duyh030774@gmail.com>
2025-09-24 15:32:38 +0700

    Add files via upload

---
+++ Lab4/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    public class Person
+    {
+        public string Name { get; }
+        public int Age { get; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
---
+++ Lab4/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Lab4
+{
+    internal class Program
+    {
+        delegate int ComparisonDelegate(Person p1, Person p2);
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>
+            {
+                new Person("John", 25),
+                new Person("Jane", 30),
+                new Person("Bob", 35),
+            };
+            ComparisonDelegate compareByName = (p1, p2) => p1.Name.CompareTo(p2.Name);
+            ComparisonDelegate compareByAge = (p1, p2) => p2.Age.CompareTo(p1.Age);
+            Console.WriteLine("Sorting by Name:");
+            SortAndDisplay(people, compareByName);
+            Console.WriteLine("\nSorting by Age:");
+            SortAndDisplay(people, compareByAge);
+        }
+        static void SortAndDisplay(List<Person> people, ComparisonDelegate comparison)
+        {
+            var sortedPeople = people.OrderBy(p => p, Comparer<Person>.Create((p1, p2) => comparison(p1, p2))).ToList();
+            foreach (var person in sortedPeople)
+            {
+                Console.WriteLine($"{person.Name}, {person.Age}");
+            }
+        }
+    }
+}
