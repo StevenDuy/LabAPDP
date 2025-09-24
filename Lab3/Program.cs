5d54c0af2d0ed3be72a929089c5bec45c450766d
StevenDuy <duyh030774@gmail.com>
2025-09-24 13:39:17 +0700

    Add files via upload

---
+++ Lab3/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Lab3
+{
+    internal class StudentProgram
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student();
+            Student student2 = new Student(1, "John", "Doe", new DateTime(2000, 1, 1));
+            Console.WriteLine("Stuent 1 details:");
+            student1.DisplayStudent();
+            Console.WriteLine();
+            Console.WriteLine("Student 2 details:");
+            student2.DisplayStudent();
+
+            Console.WriteLine();
+            Console.WriteLine("Total students: " + Student.TotalStudents);
+
+        }
+    }
+}
---
+++ Lab3/Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    internal class Student
+    {
+        public static int TotalStudents { get; set; } = 0;
+        int StudentId { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        DateTime? DayOfBirth { get; set; }
+        public DateTime EnrollmentDate { get; set; } = DateTime.Now;
+
+        public Student()
+        {
+            //constructor
+            TotalStudents++;
+            StudentId = 0;
+            FirstName = "John";
+            LastName = "Doe";
+            DayOfBirth = null;
+        }
+        public Student(int studentId, string firstName, string lastName, DateTime? dayOfBirth)
+        {
+            TotalStudents++;
+            StudentId = studentId;
+            FirstName = firstName;
+            LastName = lastName;
+            DayOfBirth = dayOfBirth;
+        }
+        public void DisplayStudent()
+        {
+            Console.WriteLine($"Student ID: {StudentId}");
+            Console.WriteLine($"Name: {FirstName} {LastName}");
+            Console.WriteLine($"Date of Birth: {(DayOfBirth.HasValue ? DayOfBirth.Value.ToShortDateString() : "N/A")}");
+            Console.WriteLine($"Enrollment Date: {EnrollmentDate.ToShortDateString()}"); // Commented out, EnrollmentDate not defined
+        }
+    }
+}
